92d6d551febb73bed31ad48b138c873615a04c8a
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-09-18 21:17:48 +0700

    readline

--- index.js
+++ index.js
@@ -111,6 +111,21 @@ var matchedOnListProduct = function(term){
 
 console.log(matchedOnListProduct('Hành lá'));
 
+/* call out repl to work with user */
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log('Thank you for your valuable feedback:', answer);
+
+  rl.close();
+});
+
 
 
 
